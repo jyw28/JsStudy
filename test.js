92195a2fb77959ddab315ad081e8b02c729673e7
dgsw8th36 <dgsw8th36@23s036.local>
2023-06-01 21:45:38 +0900

    study

---
+++ test.js
@@ -0,0 +1,21 @@
+const num1 = 2;
+let num2 = 3;
+// let는 동적
+// const는 정적
+// var도 있긴 하지만 잘 쓰지 않음
+
+//실행할 때는 터미널에서 node (파일이름)
+// npm init
+// npm i(install) express
+
+
+
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1%num2);
+
+
+// === 는 타입까지 비교
+// C랑 거의 같음
